a58fad01611c490a07673192b0122c33e2369310
Greyson Christoforo <grey@christoforo.net>
2020-11-23 12:36:10 +0000

    remove setuptools_scm version discovery

--- src/plotter/__init__.py
+++ src/plotter/__init__.py
@@ -1,11 +1,4 @@
-from setuptools_scm import get_version
 from pkg_resources import get_distribution, DistributionNotFound
-
-# get version if running from git folder
-try:
-    __version__ = get_version(root="..", relative_to=__file__)
-except LookupError:
-    __version__ = None
 
 # get version if package has been installed
 if __version__ is None:
