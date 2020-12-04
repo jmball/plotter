77c4528d9bf7f0836e906e0e4604bceed5d06d81
Greyson Christoforo <grey@christoforo.net>
2020-12-04 21:20:55 +0000

    add systemd service unit files

--- launch_em_all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-parallel ::: "python -m plotter.eqe_plotter" "python -m plotter.it_plotter" "python -m plotter.iv_plotter" "python -m plotter.mppt_plotter" "python -m plotter.vt_plotter" 
